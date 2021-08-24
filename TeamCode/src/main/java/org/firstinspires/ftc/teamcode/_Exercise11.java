e01ceb8d62a60fe76554eaf9dbb2eed463488df3
arjun punichithaya <arjunrp09@gmail.com>
2021-08-24 10:13:43 -0400

    Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Exercise11.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp(group = "Linear OpMode")
+public class _Exercise11 extends LinearOpMode {
+    _ProgrammingBoard11 board = new _ProgrammingBoard11();
+
+    @Override
+    public void runOpMode() {
+        board.init(hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Our Heading (DEGREES)", board.getHeading(AngleUnit.DEGREES));
+            telemetry.addData("Our Heading (RADIANS)", board.getHeading(AngleUnit.RADIANS));
+        }
+    }
+}
